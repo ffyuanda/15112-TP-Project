07b467639d3b4d0c3b350e3bc00c425262ec70e9
ffyuanda <46557895+ffyuanda@users.noreply.github.com>
2019-08-03 15:17:26 -0400

    The first

--- Xilliards/src/Table.py
+++ Xilliards/src/Table.py
@@ -1,12 +1,108 @@
+# This is the function in control of the billiards table
 
-# This is the function in control of the table of billiard
-
-
-
+class table(object):
 
 
+    def __init__(self, data, width, height):
+        self.width = width
+        self.height = height
+        self.centerX = data.width // 2
+        self.centerY = data.height // 2
+        self.color = "green"
+
+    def draw(self, canvas):
+        canvas.create_rectangle(self.centerX - self.width // 2,
+                                self.centerY - self.height // 2,
+                                self.centerX + self.width // 2,
+                                self.centerY + self.height // 2,
+                                fill = self.color
+                                )
+
+class pocket(object):
+
+    def __init__(self):
+
+        pass
 
 
 
 
 
+
+# Basic Animation Framework
+
+from tkinter import *
+
+####################################
+# customize these functions
+####################################
+
+def init(data):
+    # load data.xyz as appropriate
+    data.table = table(data, data.width, data.height)
+    pass
+
+def mousePressed(event, data):
+    # use event.x and event.y
+    pass
+
+def keyPressed(event, data):
+    # use event.char and event.keysym
+    pass
+
+def timerFired(data):
+    pass
+
+def redrawAll(canvas, data):
+    # draw in canvas
+    data.table.draw(canvas)
+    pass
+
+####################################
+# use the run function as-is
+####################################
+
+def run(width=300, height=300):
+    def redrawAllWrapper(canvas, data):
+        canvas.delete(ALL)
+        canvas.create_rectangle(0, 0, data.width, data.height,
+                                fill='white', width=0)
+        redrawAll(canvas, data)
+        canvas.update()
+
+    def mousePressedWrapper(event, canvas, data):
+        mousePressed(event, data)
+        redrawAllWrapper(canvas, data)
+
+    def keyPressedWrapper(event, canvas, data):
+        keyPressed(event, data)
+        redrawAllWrapper(canvas, data)
+
+    def timerFiredWrapper(canvas, data):
+        timerFired(data)
+        redrawAllWrapper(canvas, data)
+        # pause, then call timerFired again
+        canvas.after(data.timerDelay, timerFiredWrapper, canvas, data)
+    # Set up data and call init
+    class Struct(object): pass
+    data = Struct()
+    data.width = width
+    data.height = height
+    data.timerDelay = 100 # milliseconds
+    root = Tk()
+    init(data)
+    # create the root and the canvas
+    canvas = Canvas(root, width=data.width, height=data.height)
+    canvas.configure(bd=0, highlightthickness=0)
+    canvas.pack()
+    # set up events
+    root.bind("<Button-1>", lambda event:
+                            mousePressedWrapper(event, canvas, data))
+    root.bind("<Key>", lambda event:
+                            keyPressedWrapper(event, canvas, data))
+    timerFiredWrapper(canvas, data)
+    # and launch the app
+    root.mainloop()  # blocks until window is closed
+    print("bye!")
+
+run(800, 600)
--- Xilliards/venv/Scripts/Activate.ps1
+++ Xilliards/venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\ThinkPad\Xilliards\venv"
+$env:VIRTUAL_ENV="C:\Users\ThinkPad\Xilliard\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
